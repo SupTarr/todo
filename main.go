43e089c79881e4f6933dfbe62e1d5dc7cf48c511
supakrit.c <supakrit.c@arise.tech>
2024-01-08 23:36:19 +0700

    feat: add todo to sqlite

--- main.go
+++ main.go
@@ -7,16 +7,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+
+	"github.com/SupTarr/todo/todos"
 )
-
-type User struct {
-	gorm.Model
-	Name string
-}
-
-type UserHandler struct {
-	db *gorm.DB
-}
 
 func main() {
 	db, err := gorm.Open(sqlite.Open("todos.db"), &gorm.Config{})
@@ -24,24 +17,16 @@ func main() {
 		panic("Failed to connect database")
 	}
 
-	db.AutoMigrate(&User{})
-
-	db.Create(&User{Name: "Supakrit"})
+	db.AutoMigrate(&todos.Todo{})
 
 	r := gin.Default()
 	r.GET("/ping", PingPongHandler)
 
-	userHandler := UserHandler{db: db}
-	r.GET("/users", userHandler.GetUser)
+	todoHandler := todos.NewTodoHandler(db)
+	r.POST("/todos", todoHandler.NewTask)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func PingPongHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "pong"})
-}
-
-func (h *UserHandler) GetUser(c *gin.Context) {
-	var u User
-	h.db.First(&u)
-	c.JSON(200, u)
 }
---
+++ todos/todos.go
@@ -0,0 +1,47 @@
+package todos
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	gorm.Model
+	Title string `json:"text" binding:"required"`
+}
+
+func (Todo) TableName() string {
+	return "todos"
+}
+
+type TodoHandler struct {
+	db *gorm.DB
+}
+
+func NewTodoHandler(db *gorm.DB) *TodoHandler {
+	return &TodoHandler{db: db}
+}
+
+func (t *TodoHandler) NewTask(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	r := t.db.Create(&todo)
+	if err := r.Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"ID": todo.Model.ID,
+	})
+}
