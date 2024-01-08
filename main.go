a1162537ad0ee346a1cbb34192a0b205209c7249
supakrit.c <supakrit.c@arise.tech>
2024-01-08 22:31:04 +0700

    feat: add connection to sqlite3

--- main.go
+++ main.go
@@ -5,9 +5,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type User struct {
+	gorm.Model
+	Name string
+}
+
 func main() {
+	db, err := gorm.Open(sqlite.Open("todos.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	db.Create(&User{Name: "Supakrit"})
+
 	r := gin.Default()
 	r.GET("/ping", pingPongHandler)
 	log.Fatal(http.ListenAndServe(":8080", r))
