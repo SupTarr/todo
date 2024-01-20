14d9c794871c213a9f48d3318330838010f483ef
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2024-01-20 21:58:41 +0700

    feat: add mongoDB interface

--- store/mongodb.go
+++ store/mongodb.go
@@ -1,7 +1,32 @@
 package store
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+
+	"github.com/SupTarr/todo/todos"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type MongoDBStore struct {
 	*mongo.Collection
+}
+
+func NewMongoDBStore(db *mongo.Collection) *MongoDBStore {
+	return &MongoDBStore{Collection: db}
+}
+
+func (s *MongoDBStore) NewTodo(todo *todos.Todo) error {
+	_, err := s.Collection.InsertOne(context.Background(), todo)
+	return err
+}
+
+func (s *MongoDBStore) GetTodos(todo *[]todos.Todo) error {
+	_, err := s.Collection.Find(context.Background(), todo)
+	return err
+}
+
+func (s *MongoDBStore) DeleteTodo(todo *todos.Todo, id int) error {
+	_, err := s.Collection.DeleteOne(context.Background(), bson.D{"ID", id})
+	return err
 }
